39a2c764d15ed7f63eb0f50407bdc3d4510d1fb9
Kaseriu <arthurmatuszek@gmail.com>
2023-06-06 14:50:35 +0200

    Exo 3

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,26 +1,63 @@
 package org.example;
 
 public class Calculator {
+    private final String operation;
     private final String[] lines;
 
-    public Calculator(String[] lines) {
-        this.lines = lines;
+    public Calculator(String filePath, String operation) {
+        this.operation = operation;
+        Parser parser = new Parser(filePath);
+        this.lines = parser.parseFile();
     }
 
-    public void add() {
-        int sum = 0;
+    public void calculate() {
+        switch (this.operation) {
+            case "+" -> this.addition();
+            case "-" -> this.soustraction();
+            case "*" -> this.multiplication();
+            default -> System.out.println("Operation not supported");
+        }
+    }
 
-        for (int i = 0; i < this.lines.length; i++) {
+    public void addition() {
+        int sum = Integer.parseInt(this.lines[0]);
+        System.out.println(this.lines[0]);
+
+        for (int i = 1; i < this.lines.length; i++) {
             sum += Integer.parseInt(this.lines[i]);
-            if (i == 0) {
-                System.out.println(this.lines[i]);
-            } else {
-                System.out.print("+" + this.lines[i]);
-                System.out.println(" (= " + sum + ")");
-            }
+            System.out.print("+" + this.lines[i]);
+            System.out.println(" (= " + sum + ")");
         }
 
         System.out.println("-------");
         System.out.println("total = " + sum + " (addition)");
+    }
+
+    public void soustraction() {
+        int sum = Integer.parseInt(this.lines[0]);
+        System.out.println(this.lines[0]);
+
+        for (int i = 1; i < this.lines.length; i++) {
+            sum -= Integer.parseInt(this.lines[i]);
+            System.out.print("-" + this.lines[i]);
+            System.out.println(" (= " + sum + ")");
+        }
+
+        System.out.println("-------");
+        System.out.println("total = " + sum + " (soustraction)");
+    }
+
+    public void multiplication() {
+        int sum = Integer.parseInt(this.lines[0]);
+        System.out.println(this.lines[0]);
+
+        for (int i = 1; i < this.lines.length; i++) {
+            sum *= Integer.parseInt(this.lines[i]);
+            System.out.print("*" + this.lines[i]);
+            System.out.println(" (= " + sum + ")");
+        }
+
+        System.out.println("-------");
+        System.out.println("total = " + sum + " (multiplication)");
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,8 @@
 package org.example;
 
-import java.io.File;
-
 public class Main {
     public static void main(String[] args) {
-        Parser parser = new Parser("src/main/java/org/example/numbers.txt");
-        String[] lines = parser.parseFile();
-        Calculator calculator = new Calculator(lines);
-        calculator.add();
+        Calculator calculator = new Calculator("src/main/resources/numbers.txt", "*");
+        calculator.calculate();
     }
 }
